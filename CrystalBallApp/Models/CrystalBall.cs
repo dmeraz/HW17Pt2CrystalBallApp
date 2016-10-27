e3a0c00323255f954127cb0b44bb7f3c3df44f9c
dmeraz <darin.meraz@gmail.com>
2016-10-26 22:19:33 -0400

    completed tutorial, attempt at centering prediction over img using jsfiddle example not working

---
+++ CrystalBallApp/Models/CrystalBall.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrystalBallApp.Models
+{
+    public class CrystalBall
+    {
+        //create an array of strings for responses
+        //get random number
+
+        public string getResponse()
+        {
+            string[] responses = { "You are loved", "It's best to serve soup warm", "I like ponies" };
+            //get random controller object
+            Random randomNum = new Random();
+            int num = randomNum.Next(0,responses.Length); //may be int or var
+            return responses[num];
+
+        }
+    }
+}
